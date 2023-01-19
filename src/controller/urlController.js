1058bfdc5296e499ba0b3db5fbf76865d4607849
Abhi-cal <iamabhimaity@gmail.com>
2023-01-19 16:32:35 +0530

    99% done

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,63 +1,78 @@
 const urlModel = require("../models/urlModel.js")
-const validator =require("validator")
+const validator = require("validator")
 const shortId = require("shortid")
-const {SET_ASYNC,GET_ASYNC} = require("../routes/cache")
+const axios = require('axios')
+const { SET_ASYNC, GET_ASYNC } = require("../routes/cache")
 
-const createUrl= async function(req,res){
-    try{
-        let data = req.body
-        let reqLongUrl = data.longUrl
-        if(Object.keys(data).length==0) return res.status(400).send({status:false,message:"please enter url !!"})
+const createUrl = async function (req, res) {
+    try {
 
-        if((typeof reqLongUrl)!= "string") return res.status(400).send({status:false,message:"url must be string"})
+        let reqLongUrl = req.body.longUrl
+        if (Object.keys(req.body).length == 0) return res.status(400).send({ status: false, message: "please enter url !!" })
 
-        if(!reqLongUrl.trim())return res.status(400).send({status:false,message:"please enter url first"})
+        if ((typeof reqLongUrl) != "string") return res.status(400).send({ status: false, message: "url must be string" })
 
-        if(!validator.isURL(reqLongUrl)) return  res.status(400).send({status:false,message:"enter valid Url"})
+        if (!reqLongUrl.trim()) return res.status(400).send({ status: false, message: "please enter url first" })
+        let urlData = await GET_ASYNC(`${reqLongUrl}`)
+        let url = JSON.parse(urlData)
+        if (url)
+            return res.status(200).send({
+                status: true,
+                message: "URL already shorten come from cache",
+                data: url
+            })
+        const response = await axios.get(reqLongUrl)
+            .then(() => reqLongUrl)
+            .catch(() => null)
+        if (!response) return res.status(400).send({ status: false, msg: `The URL ${reqLongUrl} is not valid` })
 
-        const findUrl = await urlModel.findOne({longUrl:reqLongUrl}).select({urlCode:1,longUrl:1,shortUrl:1,_id:0})
+        const findUrl = await urlModel.findOne({ longUrl: reqLongUrl }).select({ urlCode: 1, longUrl: 1, shortUrl: 1, _id: 0 })
 
-        if(findUrl) return res.status(200).send({status:true,data:findUrl})
+        if (findUrl) {
+            await SET_ASYNC(`${reqLongUrl}`, 100, JSON.stringify(findUrl))
+            return res.status(200).send({ status: true, message: "URL already shorten", data: findUrl })
+        }
 
         let uniqueUrlCode = shortId.generate()
         data['urlCode'] = uniqueUrlCode
-    
-        let shortUrl = `${req.protocol}://${ req.get('host')}/` + uniqueUrlCode
+
+        let shortUrl = `${req.protocol}://${req.get('host')}/` + uniqueUrlCode
         data['shortUrl'] = shortUrl
         data['longUrl'] = reqLongUrl
-        
+
         const crteateData = await urlModel.create(data)
-        let shortnerUrl = {urlCode:crteateData.urlCode,longUrl:crteateData.longUrl,shortUrl:crteateData.shortUrl}
 
-        await SET_ASYNC (`${uniqueUrlCode}`,20,JSON.stringify(data))
 
-        res.status(201).send({status:true,data:shortnerUrl})
+        let shortnerUrl = { urlCode: crteateData.urlCode, longUrl: crteateData.longUrl, shortUrl: crteateData.shortUrl }
 
-    }catch(err){
-        res.status(500).send({errorType:err.name,message:err.message})
+        res.status(201).send({ status: true, data: shortnerUrl })
+
+    } catch (err) {
+        res.status(500).send({ errorType: err.name, message: err.message })
     }
 }
 
 
-const getUrl = async function (req,res){
-    try{
+const getUrl = async function (req, res) {
+    try {
         let reqUrl = req.params.urlCode
-        let cacheUrl= await GET_ASYNC(`${reqUrl}`)
-        // console.log(cacheUrl)
-        if(cacheUrl){
-            cacheUrl = JSON.parse(cacheUrl)
-             return res.status(302).redirect(cacheUrl.longUrl)
+        let cacheUrl = await GET_ASYNC(`${reqUrl}`)
+        console.log("Abhi")
+        cacheUrl = JSON.parse(cacheUrl)
+        if (cacheUrl) {
+            return res.status(302).redirect(cacheUrl.longUrl)
         }
-        
-        const url = await urlModel.findOne({urlCode: reqUrl})
-        
-        if(!url) return res.status(404).send({status:true, message:"The URL is not found."})
 
-        await SET_ASYNC (`${reqUrl}`,20,JSON.stringify(url))
-         return res.status(302).redirect(url.longUrl)
+        const url = await urlModel.findOne({ urlCode: reqUrl })
+
+        if (!url) return res.status(404).send({ status: true, message: "The URL is not found." })
+
+        await SET_ASYNC(`${reqUrl}`, 89, JSON.stringify(url))
+        console.log("abhi")
+        return res.status(302).redirect(url.longUrl)
     }
-    catch(err){
-        res.status(500).send({errorType:err.name,message:err.message})
+    catch (err) {
+        res.status(500).send({ errorType: err.name, message: err.message })
     }
 }
-module.exports = {createUrl,getUrl}
+module.exports = { createUrl, getUrl }
--- src/routes/cache.js
+++ src/routes/cache.js
@@ -1,21 +1,29 @@
 const redis = require("redis");
 const { promisify } = require("util");
-require('dotenv').config()
-const redisClient = redis.createClient(
-    10209,
-    "redis-10209.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
-  );
-  redisClient.auth(process.env.AUTH_PASS, function (err) {
-    if (err) throw err;
-  });     
+
+
+
+const client = redis.createClient({
+  url:process.env.REDIS_URL
+})
+client.on('error',(err)=>console.log('Redis Client Error',err))
+console.log("Connected to Redis..")
+// require('dotenv').config()
+// const redisClient = redis.createClient(
+//     10209,
+//     "redis-10209.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+//     { no_ready_check: true }
+//   );
+//   redisClient.auth(process.env.AUTH_PASS, function (err) {
+//     if (err) throw err;
+//   });     
   
-  redisClient.on("connect", async function () {
-    console.log("Connected to RedisDB");
-  }); 
+//   redisClient.on("connect", async function () {
+//     console.log("Connected to RedisDB");
+//   }); 
 
   //set and get functions of redis
-  const SET_ASYNC = promisify(redisClient.SETEX).bind(redisClient);//to create data in cache memory.
-  const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);//to get data from cache memory.
+  const SET_ASYNC = promisify(client.SETEX).bind(client);//to create data in cache memory.
+  const GET_ASYNC = promisify(client.GET).bind(client);//to get data from cache memory.
 
-  module.exports ={redisClient,SET_ASYNC,GET_ASYNC}
+  module.exports ={SET_ASYNC,GET_ASYNC}
